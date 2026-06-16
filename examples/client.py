8d3ac1fdff3e664e0109b0532c2b4e2e32da0d69
koki.chin <qinhunagyi@gmail.com>
2026-06-16 15:12:54 +0900

    Update client.py

--- examples/client.py
+++ examples/client.py
@@ -8,14 +8,15 @@ from iox2redis import redis_for
 def main() -> int:
     service = sys.argv[1] if len(sys.argv) > 1 else "/your/topic/to/iox2_server/"
     r = redis_for(host=service, decode_responses=True)
-
-    print("PING ->", r.ping())
-    print("SET plain ->", r.set("plain", "hello"))
-    print("GET plain ->", r.get("plain"))
-
+    res = "PING -> "+str(r.ping())
+    res += "\nSET plain -> "+str(r.set("plain", "hello"))
+    res += "\nGET plain -> "+str(r.get("plain"))
     doc = {"name": "Ada", "age": 37, "tags": ["math", "computing"]}
-    print("SET JSON ->", r.set_json("user:1", doc))
-    print("GET JSON ->", r.get_json("user:1"))
+    res += "\nSET JSON -> "+str(r.set_json("user:1", doc))
+    res += "\nGET JSON -> "+str(r.get_json("user:1"))
+    print("#####################################")
+    print(res)
+    print("#####################################")
     return 0
 
 
